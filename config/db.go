e53b85a08a6d52fba2a44cb4ef0ab26b301f60b7
Adham <adham4603@gmail.com>
2025-07-15 19:13:28 +0400

    checking docs with config/db.go

--- config/db.go
+++ config/db.go
@@ -1,3 +1,19 @@
+// Package config provides application configuration utilities.
+//
+// It includes database connection management using sqlx with PostgreSQL.
+// The ConnectDB function initializes a global sqlx.DB instance that is used
+// throughout the application to interact with the database.
+//
+// Example usage:
+//
+//    //     err := config.DB.Get(&user, "SELECT * FROM users WHERE id=$1", 1)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//
+// Note: Update the PostgreSQL connection string in ConnectDB() with your
+// actual database credentials and settings.
+
 package db
 
 import (
